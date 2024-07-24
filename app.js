5bdde0a088fddd2bb100a83dfdab850c5670c2a3
SimonK44 <158204740+SimonK44@users.noreply.github.com>
2024-07-24 14:36:03 +0200

    Connexion Mongo

--- app.js
+++ app.js
@@ -1,87 +1,125 @@
 // Instancier l'application serveur
 const express = require('express');
 const app = express();
-
-// Ecoute du serveur
-app.listen(3000, () => {
-    console.log("Le serveur a démarré");
-});
+const { v4: uuidv4 } = require('uuid');
 
 // Middleware pour autoriser express à recevoir des données en JSON dans le body
 app.use(express.json());
 
-// MOCK : Déclaration d'un tableau d'articles
-let articles = [
-    { id: 1, title: 'Premier article', content: 'Contenu du premier article', author: 'Isaac' },
-    { id: 2, title: 'Deuxième article', content: 'Contenu du deuxième article', author: 'Sanchez' },
-    { id: 3, title: 'Troisième article', content: 'Contenu du troisième article', author: 'Toto' }
-];
+//------------------------BDD------------------------//
+
+// Importer Mongoose
+const mongoose = require('mongoose');
+
+// Ecouter quand la connexion success
+mongoose.connection.once('open', () => {
+        console.log(`Connecté à la bdd`)
+});
+
+// Ecouter quand la connexion plante
+mongoose.connection.on('error', (err) => {
+    console.log(`Erreur de bdd : ${err}`);
+});
+
+// Se connecter à mongo db
+mongoose.connect("mongodb://localhost:27017/db_article")
+
+// Déclaration du modele
+const Article = mongoose.model('Article', { uid: String, title : String, content : String, author : String }, 'articles');
+
+//------------------------BDD------------------------//
+
 
 // Route get vers tous les articles
-app.get('/articles', (req, res) => {
-    //Le .json parse directement les données
+app.get('/articles', async (req, res) => {
+    const articles = await Article.find();
     return res.json(articles);
 });
 
+
 // Route get vers 1 article par id
-app.get('/article/:id', (req, res) => {
-    //Number = parseInt
-    const id = Number(req.params.id);
-    const article = articles.find(article => article.id === id);
-    if (!article) {
-        return res.status(404).send('Article non trouvé');
+app.get('/article/:id', async (req, res) => {    
+    const idParam = req.params.id;
+
+    const foundArticle = await Article.findOne({ uid : idParam});
+
+    if (!foundArticle) {
+        return res.json({message : `L'article n'éxiste pas`});
     }
-    return res.json(article);
+    return res.json(foundArticle);
 });
 
+
 // Route post pour sauvegarder un nouvel article
-app.post('/save-article', (req, res) => {
+app.post('/save-article', async (req, res) => {
 
     //Récupérer l'article envoyé en json 
     const articleJSON = req.body;
 
     let foundArticle = null;
 
+    //-------------------------
+    // EDITION
+    //-------------------------
     //Est-ce qu'on a un id envoyer dans le json
-    if (articleJSON.id != undefined || articleJSON) {
+    if (articleJSON.id != undefined || articleJSON.id) {
+        
         //Essayer de trouver un article existant
-        foundArticle = articles.find(article => article.id === articleJSON.id);
-    }
+        foundArticle = await Article.findOne({ uid : id });
 
-    //Si je trouve je modifie les nouvelles
-    if (foundArticle) {
+        //Si je trouve l'article à modifier
+        if (!foundArticle) {
+            return res.json(`Impossible de modifier un article inexistant`)
+        }
         foundArticle.title = articleJSON.title;
         foundArticle.content = articleJSON.content;
         foundArticle.author = articleJSON.author;
 
-        return res.json(`l'article a été modifié avec succès`);
+        // Sauvegarder en base
+        await foundArticle.save();
+
+        return res.json(`Article modifié avec succès`);
     }
 
-    //Sinon par défaut je crée
-    articles.push(articleJSON);
+    //-------------------------
+    // CREATION
+    //-------------------------
+    //Instancier un article Mongo
+    const createdArticle = await Article.create(articleJSON);  
 
-    const id = articleJSON.id;
+    //Générer l'ID
+    createdArticle.uid = uuidv4();
 
-    return res.json(`L'article avec l'id ${id} à été créé avec succès`);
+    //Sauvegarder en base
+    await createdArticle.save();
+
+    return res.json(`Article créé avec succès`);
 });
 
+
 // Route delete pour supprimer un article par id
-app.delete('/article/:id', (req, res) => {
+app.delete('/article/:id', async (req, res) => {
 
     //Récup indice converti en nombre
-    const id = Number(req.params.id);
+    const id = req.params.id;
 
-    //Trouver l'index
-    const indexArticle = articles.findIndex(article => article.id === id);
+    //Trouver un index
+    const foundArticle = await Article.findOne({ uid : id });
 
-    if (indexArticle < 0) {
+    if (!foundArticle) {
         return res.json(`Article non trouvé`);
     }
 
-    //Supprimer 1 élément à partir de l'index
-    articles.splice(indexArticle, 1);
+    //Supprimer 1 élément
+    await foundArticle.deleteOne();
 
-    return res.send(`suppression article ${id}`);
+    return res.json(`suppression article ${id}`);
+});
+
+
+// Ecoute du serveur
+app.listen(3000, () => {
+    console.log("Le serveur a démarré");
 });
 
 
