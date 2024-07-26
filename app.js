bd07a1bf67b396966e2a871f9752c3d340676f13
SimonK44 <158204740+SimonK44@users.noreply.github.com>
2024-07-26 14:32:21 +0200

    jwt

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 const express = require('express');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+//Clé secrete
+const JWT_SECRET = "cle_secrete";
 
 // Middleware pour autoriser express à recevoir des données en JSON dans le body
 app.use(express.json());
+app.use(cors());
 
 /**
  * Fonction utilitaire pour retourner une structure de réponse métier 
@@ -15,8 +21,9 @@ app.use(express.json());
  * @returns 
  */
 function responseService(res, code, message, data) {
-    return res.json({ code : code, message : message, data : data});
-}
+    return res.json({ code : code, message : message, data : data });
+};
+
 
 //------------------------BDD------------------------//
 
@@ -38,9 +45,38 @@ mongoose.connect("mongodb://localhost:27017/db_article")
 
 // Déclaration du modele
 const Article = mongoose.model('Article', { uid: String, title : String, content : String, author : String }, 'articles');
+const User = mongoose.model('User', { email : String, password : String }, 'users');
 
 //------------------------BDD------------------------//
 
+// Middleware
+function authMiddleware(req, res, next) {
+
+    // Si token null alors erreur
+    if (req.headers.authorization == undefined || !req.headers.authorization) {
+        return res.json({ message: "Token null" });
+    }
+
+    // Extraire le token (qui est bearer)
+    const token = req.headers.authorization.substring(7);
+
+    // par defaut le result est null
+    let result = null;
+
+    // Si reussi à générer le token sans crash
+    try {
+        result = jwt.verify(token, JWT_SECRET);
+    } catch {
+    }
+
+    // Si result null donc token incorrect
+    if (!result) {
+        return res.json({ message: "token pas bon" });
+    }
+
+    //On passe le Middleware
+    return next();
+}
 
 // Route get vers tous les articles
 app.get('/articles', async (req, res) => {
@@ -64,8 +100,8 @@ app.get('/article/:id', async (req, res) => {
 
 
 // Route post pour sauvegarder un nouvel article
-app.post('/save-article', async (req, res) => {
-
+app.post('/save-article', authMiddleware, async (req, res) => {
+      
     //Récupérer l'article envoyé en json 
     const articleJSON = req.body;
 
@@ -116,11 +152,12 @@ app.post('/save-article', async (req, res) => {
     await createdArticle.save();
 
     return responseService(res, '200', 'Article ajouté avec succès', createdArticle);
+
 });
 
 
 // Route delete pour supprimer un article par id
-app.delete('/article/:id', async (req, res) => {
+app.delete('/article/:id', authMiddleware, async (req, res) => {
 
     //Récup indice converti en nombre
     const id = req.params.id;
@@ -136,6 +173,24 @@ app.delete('/article/:id', async (req, res) => {
     await foundArticle.deleteOne();
 
     return responseService(res, '200', `L'article ${id} a été supprimé avec succès`, foundArticle);
+});
+
+
+app.post('/auth', async (req, res) => {
+
+    //Tester le couple email / mdp
+    const loggedUser = await User.findOne({ email : req.body.email, password : req.body.password});
+
+    //Si mauvais
+    if (!loggedUser) {
+        return responseService(res, '701', `Couple email/password incorrect`, null);
+    }
+
+    //Se connecter (génerer un token)
+    const token = jwt.sign({ email : loggedUser.email }, JWT_SECRET, { expiresIn : '3 hours' });
+ 
+    // Retourner la réponse json
+    return responseService(res, '202', `Connecté avec succès`, token);
 });
 
 
